241821bb2b96ef9fe138b675e4293debcad61e19
Bryan Dedeurwaerder <23252092+BryanDedeur@users.noreply.github.com>
2020-12-11 02:51:01 -0800

    Release backup

---
+++ Assets/GestureCapturing/Scripts/CameraSwapper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class CameraSwapper : MonoBehaviour
+{
+    public List<Camera> cameras;
+    private int index = 0;
+
+    void Update()
+    {
+        if (SteamVR_Actions._default.Camera.GetStateDown(SteamVR_Input_Sources.Any))
+        {
+            cameras[index].gameObject.SetActive(false);
+            index++;
+            if (index > cameras.Count - 1)
+            {
+                index = 0;
+            }
+            cameras[index].gameObject.SetActive(true);
+        }
+    }
+}
+
